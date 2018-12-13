148a87b9fb9c350d47c6152d8cbc1189a09c0682
George Ashton <george@georges-mac.local>
2018-12-13 16:08:51 +0000

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by George on 13/12/2018.
+//  Copyright © 2018 George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
